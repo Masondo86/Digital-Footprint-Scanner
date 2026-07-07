3480406d3c94491deed2ee931b1aba31cf0addd7
Linda Masondo <lmasondo11@gmail.com>
2026-07-07 12:41:02 +0200

    Delete api/digital-footprint/breach directory

--- api/digital-footprint/breach/route.ts
+++
@@ -1,8 +0,0 @@
-import { checkEmailBreaches } from '@/app/services/hibp';
-export async function GET(req: Request) {
-  const { searchParams } = new URL(req.url);
-  const email = searchParams.get('email');
-  if (!email) return new Response('Email required', { status: 400 });
-  const data = await checkEmailBreaches(email);
-  return Response.json(data);
-}
